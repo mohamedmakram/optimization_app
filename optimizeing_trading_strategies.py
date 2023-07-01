b1113470d65772509bac24357e5b3976ad469896
Mohamed Makram <mohamedmakram202@gmail.com git config --global user.name mohamed>
2023-07-01 22:21:02 +0300

    change the column names as the collector

--- optimizeing_trading_strategies.py
+++ optimizeing_trading_strategies.py
@@ -20,47 +20,49 @@ if upload_files:
    # Merge the DataFrames into one DataFrame
    # data = pd.merge(*df_list, on='SequenceNo', how='inner')
 
-      strategy = df['MagicNo.'].unique()
-   # convert TimeStamp column to datetime type
-      df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
-      
+   strategy = df['MagicNo.'].unique()
+# convert TimeStamp column to datetime type
+   df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
 
-
-      # fig = px.line(df, x='TimeStamp', y='Closed P/L', color='MagicNo.', markers=True)
-      # fig = go.Figure(data=go.Scatter(x=df['TimeStamp'], y=df['Closed P/L'], ))
-
-      fig = go.Figure()
-
-      for i in strategy:
-         df_strategy = df[df['MagicNo.'] == i]
-         fig.add_trace(go.Scatter(x=df_strategy['TimeStamp'], y=df_strategy['Closed P/L'],
-            name=str(i),
-
-         ))
-
-         # Edit the layout
-         fig.update_layout(title='strategy Comparison',
-                   xaxis_title='TimeStamp',
-                   yaxis_title='Closed P/L')
-
-
-
-      # # Magic No 6835025
-      # trace1_hourly_profit = go.Bar(
-      #    x = hourly_profit.index, 
-      #    y=hourly_profit['Profit'],
-      #    name = 'Profit Close'
-      # )
-
-      # figures_hourly_profit = [trace1_hourly_profit]
-      # layout = go.Layout(barmode = 'group')
-      # fig_hourly_profit = go.Figure(data = figures_hourly_profit, layout = layout)
-
-
-      # fig_hourly_profit.update_layout(title=dict(text=f"P/L hourly {strategy}"),
-      #    xaxis = dict(
-      #       tickmode = 'linear')
-      # )
-
-      st.plotly_chart(fig)
+   df.rename(columns={'Closed P/L': 'ClosedP/L', 'Profit': 'FloatingProfit'}, inplace=True)
    
+
+
+   # fig = px.line(df, x='TimeStamp', y='Closed P/L', color='MagicNo.', markers=True)
+   # fig = go.Figure(data=go.Scatter(x=df['TimeStamp'], y=df['Closed P/L'], ))
+
+   fig = go.Figure()
+
+   for i in strategy:
+      df_strategy = df[df['MagicNo.'] == i]
+      fig.add_trace(go.Scatter(x=df_strategy['TimeStamp'], y=df_strategy['ClosedP/L'],
+         name=str(i),
+
+      ))
+
+      # Edit the layout
+      fig.update_layout(title='strategy Comparison',
+                  xaxis_title='TimeStamp',
+                  yaxis_title='Closed P/L')
+
+
+
+   # # Magic No 6835025
+   # trace1_hourly_profit = go.Bar(
+   #    x = hourly_profit.index, 
+   #    y=hourly_profit['Profit'],
+   #    name = 'Profit Close'
+   # )
+
+   # figures_hourly_profit = [trace1_hourly_profit]
+   # layout = go.Layout(barmode = 'group')
+   # fig_hourly_profit = go.Figure(data = figures_hourly_profit, layout = layout)
+
+
+   # fig_hourly_profit.update_layout(title=dict(text=f"P/L hourly {strategy}"),
+   #    xaxis = dict(
+   #       tickmode = 'linear')
+   # )
+
+   st.plotly_chart(fig)
+
