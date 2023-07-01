e33ac9e164881466e33166496473dbe1dd381c93
Mohamed Makram <mohamedmakram202@gmail.com git config --global user.name mohamed>
2023-07-01 21:48:02 +0300

    changes the chart to compare between the strategies with a line chart

--- optimizeing_trading_strategies.py
+++ optimizeing_trading_strategies.py
@@ -11,50 +11,56 @@ st.title('Optimizeing Trading Strategies')
 upload_files = st.file_uploader('Choose a CSV file', type='csv', accept_multiple_files=True)
 
 if upload_files:
+   df = pd.DataFrame()
 
-    # Read each file into a DataFrame
-   # df = pd.DataFrame()
-   try:
+   for file in upload_files: 
+      df_list = pd.read_csv(file)
+      df = pd.concat([df_list, df], axis=0)
 
-      for file in upload_files:
-         df = pd.read_csv(file)
-         # df = pd.concat([df_list, df], axis=0)
+   # Merge the DataFrames into one DataFrame
+   # data = pd.merge(*df_list, on='SequenceNo', how='inner')
 
-      # Merge the DataFrames into one DataFrame
-      # data = pd.merge(*df_list, on='SequenceNo', how='inner')
-
-         strategy = df['MagicNo'].unique()
-      # convert TimeStamp column to datetime type
-         df['Open Time'] = pd.to_datetime(df['Open Time'])
-
-         # Parameters
-         # extract hours
-         df['hourly'] = df['Open Time'].dt.hour
-         # extract days
-         df['days'] = df['Open Time'].dt.day_name()
-
-         # profit data to plot
-         # hours profit
-         
-         hourly_profit = df.groupby(['hourly'])[['Profit']].agg('sum')
-         
-         # Magic No 6835025
-         trace1_hourly_profit = go.Bar(
-            x = hourly_profit.index, 
-            y=hourly_profit['Profit'],
-            name = 'Profit Close'
-         )
-
-         figures_hourly_profit = [trace1_hourly_profit]
-         layout = go.Layout(barmode = 'group')
-         fig_hourly_profit = go.Figure(data = figures_hourly_profit, layout = layout)
+      strategy = df['MagicNo.'].unique()
+   # convert TimeStamp column to datetime type
+      df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
+      
 
 
-         fig_hourly_profit.update_layout(title=dict(text=f"P/L hourly {strategy}"),
-            xaxis = dict(
-               tickmode = 'linear')
-         )
+      # fig = px.line(df, x='TimeStamp', y='Closed P/L', color='MagicNo.', markers=True)
+      # fig = go.Figure(data=go.Scatter(x=df['TimeStamp'], y=df['Closed P/L'], ))
 
-         st.plotly_chart(fig_hourly_profit)
-   except:
-      print('this is Open file ')
+      fig = go.Figure()
+
+      for i in strategy:
+         df_strategy = df[df['MagicNo.'] == i]
+         fig.add_trace(go.Scatter(x=df_strategy['TimeStamp'], y=df_strategy['Closed P/L'],
+            name=str(i),
+
+         ))
+
+         # Edit the layout
+         fig.update_layout(title='strategy Comparison',
+                   xaxis_title='TimeStamp',
+                   yaxis_title='Closed P/L')
+
+
+
+      # # Magic No 6835025
+      # trace1_hourly_profit = go.Bar(
+      #    x = hourly_profit.index, 
+      #    y=hourly_profit['Profit'],
+      #    name = 'Profit Close'
+      # )
+
+      # figures_hourly_profit = [trace1_hourly_profit]
+      # layout = go.Layout(barmode = 'group')
+      # fig_hourly_profit = go.Figure(data = figures_hourly_profit, layout = layout)
+
+
+      # fig_hourly_profit.update_layout(title=dict(text=f"P/L hourly {strategy}"),
+      #    xaxis = dict(
+      #       tickmode = 'linear')
+      # )
+
+      st.plotly_chart(fig)
+   
